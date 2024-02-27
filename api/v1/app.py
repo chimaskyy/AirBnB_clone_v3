f01c991641a6145ddbf5694ffedd6c22d7de8beb
Joe Egboka <joeegboka@gmail.com>
2024-02-27 23:54:24 +0100

    add CORS permission

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 import os
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from api.v1.views import app_views
 from models import storage
@@ -10,7 +11,7 @@ from models import storage
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
-
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def tear_down(exception):
