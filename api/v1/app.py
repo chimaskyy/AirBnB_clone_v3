f4ca8286fbbec7500aa16d87c4e41f3aa284e6d7
Joe Egboka <joeegboka@gmail.com>
2024-02-27 23:56:41 +0100

    add CORS permission

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def tear_down(exception):
     """handles the teardown context"""
